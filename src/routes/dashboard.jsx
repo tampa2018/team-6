c1df1218f2e2cebae7b88b3bd57ec916bf463843
Ainova98 <35618146+Ainova98@users.noreply.github.com>
2018-10-05 23:54:46 -0400

    Remove maps

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -4,7 +4,6 @@ import Person from "@material-ui/icons/Person";
 // import ContentPaste from "@material-ui/icons/ContentPaste";
 import LibraryBooks from "@material-ui/icons/LibraryBooks";
 import BubbleChart from "@material-ui/icons/BubbleChart";
-import LocationOn from "@material-ui/icons/LocationOn";
 import Notifications from "@material-ui/icons/Notifications";
 import Unarchive from "@material-ui/icons/Unarchive";
 // core components/views
@@ -13,7 +12,6 @@ import UserProfile from "views/UserProfile/UserProfile.jsx";
 import TableList from "views/TableList/TableList.jsx";
 import Typography from "views/Typography/Typography.jsx";
 import Icons from "views/Icons/Icons.jsx";
-import Maps from "views/Maps/Maps.jsx";
 import NotificationsPage from "views/Notifications/Notifications.jsx";
 import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.jsx";
 
@@ -52,13 +50,6 @@ const dashboardRoutes = [
     navbarName: "Icons",
     icon: BubbleChart,
     component: Icons
-  },
-  {
-    path: "/maps",
-    sidebarName: "Maps",
-    navbarName: "Map",
-    icon: LocationOn,
-    component: Maps
   },
   {
     path: "/notifications",
