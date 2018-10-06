04323a6535b00f4e6bde51ae12dd3e60e403203c
Ainova98 <35618146+Ainova98@users.noreply.github.com>
2018-10-06 10:33:56 -0400

    Import merge push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { Menu2Component } from './menu2/menu2.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    DropdownComponent,
   ],
   imports: [
     BrowserModule
